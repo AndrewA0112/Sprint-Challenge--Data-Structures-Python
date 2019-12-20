f33d9802fa227053848e73502122928bc010d9b4
AndrewA0112 <andrew.mkal@outlook.com>
2019-12-20 02:15:45 -0600

    Names.py

--- names/names.py
+++ names/names.py
@@ -11,10 +11,19 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# Run time is still above 1 sec (1.44s) could use work
+for names_2 in names_2:
+    if names_2 in names_1:
+        duplicates.append(names_2)
+
+# Method used in stretch below (Uses a set, which was restricted), results in a runtime > 0.01s
+# cache = {}
+# for names_1 in names_1:
+#     cache[names_1] = names_1
+# for names_2 in names_2:
+#     if names_2 in cache:
+#         duplicates.append(names_2)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -24,3 +33,9 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish with no restrictions on techniques or data
 # structures?
+cache = {}
+for names_1 in names_1:
+    cache[names_1] = names_1
+for names_2 in names_2:
+    if names_2 in cache:
+        duplicates.append(names_2)
